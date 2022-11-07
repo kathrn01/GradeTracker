b1da72b8f363bb9e628adc97a1ca4421ce3106fe
Katharine K <kathrn.ko@gmail.com>
2022-11-07 09:38:42 -0600

    remove navigation view AllTermsView

--- GradeTracker/View/AllTermsView.swift
+++ GradeTracker/View/AllTermsView.swift
@@ -21,35 +21,32 @@ struct AllTermsView: View {
     @State var displayAddTerm = false
 
     var body: some View {
-        NavigationView {
-            VStack {
-                ScrollView {
-                    //all terms added by the user will appear as a list under the title
-                    if terms.isEmpty {
-                        Text("No terms added yet.")
-                    }
-
-                    ForEach(terms) { term in
-                        //click on a term to go to the view for that term and view/add/delete it's courses
-                        NavigationLink(destination: TermView(term: term).environment(\.managedObjectContext, viewContext)){
-                            TermListItemView(term: term)
-                        }
-                        .frame(width: nil, height: nil)
-                    }
-                } //ScrollView
-                
-                //add a term
-                Button(action: { displayAddTerm = true }) {
-                        Label("New Term", systemImage: "plus.circle")
-                            .foregroundColor(.blue)
-                            .font(.title2)
+        VStack {
+            ScrollView {
+                //all terms added by the user will appear as a list under the title
+                if terms.isEmpty {
+                    Text("No terms added yet.")
                 }
-            }//VStack
-            .padding()
-            .navigationTitle(Text("Terms"))
-            .navigationBarTitleDisplayMode(.automatic)
-        }//NavigationView
-        .navigationViewStyle(StackNavigationViewStyle())
+
+                ForEach(terms) { term in
+                    //click on a term to go to the view for that term and view/add/delete it's courses
+                    NavigationLink(destination: TermView(term: term).environment(\.managedObjectContext, viewContext)){
+                        TermListItemView(term: term)
+                    }
+                    .frame(width: nil, height: nil)
+                }
+            } //ScrollView
+            
+            //add a term
+            Button(action: { displayAddTerm = true }) {
+                    Label("New Term", systemImage: "plus.circle")
+                        .foregroundColor(.blue)
+                        .font(.title2)
+            }
+        }//VStack
+        .padding()
+        .navigationTitle(Text("Terms"))
+        .navigationBarTitleDisplayMode(.automatic)
         //this sheet will present when the user selects "add term"
         .sheet(isPresented: $displayAddTerm, content: {
             NavigationView {
